752b93cbc0fcd3afbc2e44391ffba548e8ab1d9e
Jabarlew <barlewdesign@gmail.com>
2018-08-02 21:18:25 +0200

    menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimes, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './layout.scss';
 import Map from './map';
 import Sidebar from './sidebar';
 
-const Layout = ({ lat, lng, updateLocation }) => (
-  <div className={styles.container}>
-    <header className={styles.header}>
-      <h1>
-        Neighbourhood Map
-      </h1>
-    </header>
+class Layout extends React.Component {
+  constructor(...args) {
+    super(...args);
 
-    <Sidebar updateLocation={updateLocation} />
+    this.toggleSidebar = this.toggleSidebar.bind(this);
 
-    <main>
-      <Map lat={lat} lng={lng} />
-    </main>
-  </div>
-);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  toggleSidebar() {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  }
+
+  render() {
+    const { lat, lng, updateLocation } = this.props;
+    const { isOpen } = this.state;
+
+    return (
+      <div className={`${styles.container} ${isOpen ? null : styles.on}`}>
+
+        <header className={styles.header}>
+          <button type="button" onClick={this.toggleSidebar}>
+            <FontAwesomeIcon className={styles.menuIcon} icon="bars" />
+          </button>
+          <h1>
+            Neighbourhood Map
+          </h1>
+        </header>
+
+        <Sidebar
+          updateLocation={updateLocation}
+          turnOnSidebar={this.turnOnSidebar}
+          {...this.state}
+        />
+
+        <main>
+          <Map lat={lat} lng={lng} />
+        </main>
+      </div>
+    );
+  }
+}
+
 
 Layout.propTypes = {
   lat: PropTypes.number.isRequired,
@@ -28,5 +59,5 @@ Layout.propTypes = {
   updateLocation: PropTypes.func.isRequired,
 };
 
-library.add(faTimes, faCoffee);
+library.add(faTimes, faCoffee, faBars);
 export default Layout;
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,64 +4,46 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './layout.scss';
 import locations from '../locations';
 
-class Sidebar extends React.Component {
-  constructor(...arg) {
-    super(...arg);
 
-    this.state = {
-      isOpen: false,
-    };
-  }
+const Sidebar = ({ updateLocation }) => (
+  <div>
+    <aside className={styles.sidebar}>
+      <div className={styles.inline}>
+        <h2 className={styles.sidebarTitle}>
+          Main Menu
+        </h2>
+        <button
+          type="button"
+          className={styles.closeButton}
+          onClick={() => {
 
-  turnOnSidebar() {
-    this.setState({ isOpen: true });
-  }
-
-  render() {
-    const { updateLocation } = this.props;
-    const close = this.state;
-    return (
-      <div className={close.isOpen ? styles.on : null}>
-        <aside className={styles.sidebar}>
-          <div className={styles.inline}>
-            <h2 className={styles.sidebarTitle}>
-              Main Menu
-            </h2>
-            <button
-              type="button"
-              className={styles.closeButton}
-              onClick={() => {
-                this.turnOnSidebar();
-                console.log(this.state.isOpen);
-              }}
-            >
-              <FontAwesomeIcon icon="times" />
-            </button>
-          </div>
-          <nav>
-            {/* Search */}
-            <input type="text" className={styles.input} aria-label="Search" placeholder="Filter Locations" />
-            <ul>
-              {locations.map(location => (
-                <li key={location.name}>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      updateLocation(location.position.lat, location.position.lng);
-                    }}
-                  >
-                    <FontAwesomeIcon className={styles.coffeeIcon} icon="coffee" />
-                    {location.name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </aside>
+          }}
+        >
+          <FontAwesomeIcon icon="times" />
+        </button>
       </div>
-    );
-  }
-}
+      <nav>
+        {/* Search */}
+        <input type="text" className={styles.input} aria-label="Search" placeholder="Filter Locations" />
+        <ul>
+          {locations.map(location => (
+            <li key={location.name}>
+              <button
+                type="button"
+                onClick={() => {
+                  updateLocation(location.position.lat, location.position.lng);
+                }}
+              >
+                <FontAwesomeIcon className={styles.coffeeIcon} icon="coffee" />
+                {location.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  </div>
+);
 
 Sidebar.propTypes = {
   updateLocation: PropTypes.func.isRequired,
